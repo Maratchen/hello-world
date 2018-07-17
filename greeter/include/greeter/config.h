d1d076e5ccb70af0002f5d1d44022f4a600af43b
Marat Khasanov <marat@metaquotes.net>
2018-07-17 18:00:00 +0300

    Fix build for other compilers

--- greeter/include/greeter/config.h
+++ greeter/include/greeter/config.h
@@ -1,13 +1,13 @@
 #pragma once
 
+#define GREETER_API
+
 #ifdef _MSC_VER
 #  ifdef GREETER_EXPORTS
 #    define GREETER_API __declspec(dllexport)
 #  else
 #    ifdef GREETER_IMPORTS
 #      define GREETER_API __declspec(dllimport)
-#    else
-#      define GREETER_API
 #    endif
 #  endif
 #endif
